c6986809a713f7ae3c24ce6a2cac222300c8dd8d
Enrico Fischer <enrico.fischer92@gmail.com>
2019-06-14 14:53:12 +0200

    maintenance fix

--- enfi-framework/maintenance.php
+++ enfi-framework/maintenance.php
@@ -12,13 +12,12 @@
 
 <head>
 
-    <!-- title -->
     <title><?php _e('MAINTENANCE_MODE_503', 'ef'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
-<body <?php body_class(); ?>>
+<body>
 
 <h1><?php _e('MAINTENANCE_MODE_503', 'ef'); ?></h1>
 
