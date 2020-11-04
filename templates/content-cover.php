1469fea6416495bad33f4b29b64dc8f7cceae045
Enrico Fischer <enrico.fischer92@gmail.com>
2020-11-04 13:49:16 +0100

    remove old staff

--- templates/content-cover.php
+++ templates/content-cover.php
@@ -1,10 +1,9 @@
 <section>
-    <div class="content-cover" <?php #echo ef_cover_get_bg_style(); ?>>
+    <div class="content-cover">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-lg-12">
                     <h1>
-                    <?php #echo ef_cover_get_title(); ?>
                     </h1>
                 </div>
             </div>
