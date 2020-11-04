eaec5e6a8cd91ea2c616a74955e2b038ae8cb0fd
Enrico Fischer <enrico.fischer92@gmail.com>
2020-11-04 13:50:18 +0100

    add html field

--- functions.php
+++ functions.php
@@ -6,4 +6,14 @@
 
 require_once get_template_directory().'/core/ef.php';
 
+add_action('admin_enqueue_scripts', 'codemirror_enqueue_scripts');
+ 
+function codemirror_enqueue_scripts($hook) {
+  $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'text/css'));
+  wp_localize_script('jquery', 'cm_settings', $cm_settings);
+ 
+  wp_enqueue_script('wp-theme-plugin-editor');
+  wp_enqueue_style('wp-codemirror');
+}
+
 ?>
